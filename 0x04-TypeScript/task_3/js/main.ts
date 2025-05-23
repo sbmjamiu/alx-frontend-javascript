d5e9fbc9e4a3285dfebe5aac2cadd01b644a1dc1
Jamiu Abdulhakeem <sbmjamiu@gmail.com>
2025-05-23 15:07:58 +0100

    complete task 3

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,18 @@
+/// <reference path="./crud.d.ts" />
+/// <reference path="./interface.ts" />
+import * as CRUD from "./crud.js";
+
+const row: RowElement = {
+  firstName: "Guillaume",
+  lastName: "Salva",
+};
+
+const newRowID: RowID = CRUD.insertRow(row);
+
+const updatedRow: RowElement = {
+  ...row,
+  age: 23,
+};
+
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
