787de3f79fe227bf8e5c6bbce2f2894f79e4885e
Jamiu Abdulhakeem <sbmjamiu@gmail.com>
2025-05-23 15:09:10 +0100

    complete task 5

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,30 @@
+export interface MajorCredits {
+  credits: number;
+  readonly brand: "MajorCredits";
+}
+
+export interface MinorCredits {
+  credits: number;
+  readonly brand: "MinorCredits";
+}
+
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "MajorCredits",
+  };
+}
+
+// Function to sum MinorCredits
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "MinorCredits",
+  };
+}
